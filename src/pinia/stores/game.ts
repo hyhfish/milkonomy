0c534fb730ef320f0974eee53da62f913d5597d9
whisper <470103427@qq.com>
2025-05-07 16:34:35 +0800

    feat: 添加新的市场数据源URL至useGameStore

--- src/pinia/stores/game.ts
+++ src/pinia/stores/game.ts
@@ -96,6 +96,7 @@ export const useGameStore = defineStore("game", {
       // }
       const url = import.meta.env.MODE === "development" ? "/" : "./"
       const MARKET_URLS = [
+        "https://hub.gitmirror.com/https://raw.githubusercontent.com/holychikenz/MWIApi/main/milkyapi.json",
         "https://gh-proxy.470103427.workers.dev/raw.githubusercontent.com/holychikenz/MWIApi/main/milkyapi.json",
         "https://gh-proxy.com/raw.githubusercontent.com/holychikenz/MWIApi/main/milkyapi.json",
         "https://raw.githubusercontent.com/holychikenz/MWIApi/main/milkyapi.json"
